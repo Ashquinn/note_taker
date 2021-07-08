aa7ba9147ee91d5eb7f5dd195bb74a82b37a8309
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-07-08 17:56:47 -0400

    server.js file added to repo, logic added to server file to import express

---
+++ server.js
@@ -0,0 +1,14 @@
+express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+rrequire('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
